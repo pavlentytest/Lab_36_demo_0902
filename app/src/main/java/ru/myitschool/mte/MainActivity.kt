8ce05eedcd9c08bea6b25901374c56519a37894c
Pavel <impetus@yandex.ru>
2023-02-09 11:46:08 +0300

    Corutines

--- app/src/main/java/ru/myitschool/mte/MainActivity.kt
+++ app/src/main/java/ru/myitschool/mte/MainActivity.kt
@@ -1,27 +1,44 @@
-package ru.myitschool.mte;
+package ru.myitschool.mte
 
-import android.os.Bundle;
-import android.widget.Button;
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import ru.myitschool.mte.databinding.ActivityMainBinding
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+    private lateinit var job: Job
+    private var counter = 0
 
-import ru.myitschool.mte.databinding.ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        btnStart = binding.content.startBtn
+        btnStop = binding.content.stopBtn
+        val fragments = arrayOf(FirstFragment(), ProceedingFragment())
+        val fragmentManager = supportFragmentManager
 
-public class MainActivity extends AppCompatActivity {
+        btnStart.setOnClickListener {
+             job = GlobalScope.launch {
+            //   job = CoroutineScope(Dispatchers.IO).launch {
+                while(true) {
+                    val transaction = fragmentManager.beginTransaction()
+                    val currentFragment = fragments[counter++ %2]
+                    transaction.replace(R.id.output_fragment, currentFragment)
+                    transaction.commit()
+                    delay(3000)
+                }
+            }
+        }
 
-    private ActivityMainBinding binding;
-
-    private Button btnStart, btnStop;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        btnStart = binding.content.startBtn;
-        btnStop = binding.content.stopBtn;
-
+        btnStop.setOnClickListener {
+            job.cancel()
+        }
     }
+
+
 }
